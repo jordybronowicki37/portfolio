df974bd14396ef9a370c2600a4aff738b9085358
jordybronowicki37 <74408184+jordybronowicki37@users.noreply.github.com>
2023-07-28 14:39:43 +0200

    Created the main icon

---
+++ src/components/MainTitle.vue
@@ -0,0 +1,24 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+  <div class="title">
+    JB
+  </div>
+</template>
+
+<style scoped>
+.title {
+  font-size: 30px;
+  font-weight: bold;
+  text-align: center;
+  letter-spacing: -2px;
+  width: 3rem;
+  height: 3rem;
+  margin: 0.5rem;
+  border-radius: 0.5rem;
+  background: linear-gradient(125deg, #705499, #af5b5b, #ea6d30);
+  user-select: none;
+}
+</style>
