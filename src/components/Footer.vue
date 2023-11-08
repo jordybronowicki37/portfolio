b72849bd3a267ef23f515db8b9e17fe9bdec38f3
jordy <74408184+jordybronowicki37@users.noreply.github.com>
2023-11-08 14:02:40 +0100

    Reworked footer

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,11 +1,21 @@
 <script setup lang="ts">
 import packageJson from '/package.json';
+
+const uri = document.baseURI;
+let branchName = "master";
+if (uri.includes("localhost") || uri.includes("-dev")) branchName = "dev";
 </script>
 
 <template>
   <footer>
-    <div>options</div>
+    <div>
+      <box-icon name='git-branch' size="1em" color="var(--font-color-200)" />
+      <span>{{branchName}}</span>
+    </div>
     <div>V{{ packageJson.version }}</div>
+    <button class="settings-button">
+      <box-icon type='solid' name='cog' size="1em" color="var(--font-color-200)" />
+    </button>
   </footer>
 </template>
 
@@ -14,7 +24,21 @@ footer {
   background: var(--bg-color-600);
   display: flex;
   justify-content: flex-end;
+}
+.settings-button {
+  background-color: unset;
+  border: none;
+}
+.settings-button:focus-visible {
+  outline: none;
+}
+footer>* {
+  display: flex;
+  align-items: center;
   padding: 0.2rem 0.5rem;
-  gap: 0.5rem;
+}
+footer>*:hover {
+  background-color: var(--bg-color-500);
+  user-select: none;
 }
 </style>
