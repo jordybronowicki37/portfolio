a5d80b0b8cf60eb61de02f6624b83876a927fe00
jordy <74408184+jordybronowicki37@users.noreply.github.com>
2024-03-02 21:51:02 +0100

    Added image full screen view

--- src/components/ImageTextCombinedView.vue
+++ src/components/ImageTextCombinedView.vue
@@ -1,8 +1,19 @@
 <script setup lang="ts">
+import {ref} from "vue";
+
 withDefaults(
     defineProps<{ imageOnTheLeft?: boolean }>(),
     {imageOnTheLeft: false}
 );
+const modal = ref<HTMLDialogElement>();
+function openModal() {
+  if (!modal.value) return;
+  modal.value?.showModal();
+}
+function closeModal() {
+  if (!modal.value) return;
+  modal.value?.close();
+}
 </script>
 
 <template>
@@ -13,10 +24,23 @@ withDefaults(
     <div class="left-wrapper text-wrapper">
       <slot name="text" />
     </div>
-    <div class="right-wrapper image-wrapper">
+    <div class="right-wrapper image-wrapper" @click="openModal">
       <slot name="image" />
     </div>
   </div>
+
+  <dialog class="full-screen-image-modal" ref="modal" @click="closeModal">
+    <div class="full-screen-image-wrapper" @click="$event.stopPropagation()">
+      <slot name="image" />
+    </div>
+    <div class="modal-close-button-wrapper" @click="closeModal">
+      <box-icon
+          color="var(--font-color-200)"
+          size="2em"
+          name="x"
+      />
+    </div>
+  </dialog>
 </template>
 
 <style scoped>
@@ -30,6 +54,7 @@ withDefaults(
 }
 .image-wrapper {
   width: 40%;
+  cursor: pointer;
 }
 .text-wrapper {
   width: 60%;
@@ -45,5 +70,32 @@ withDefaults(
   .text-wrapper {
     width: 100%;
   }
+}
+.full-screen-image-modal::backdrop {
+  backdrop-filter: blur(10px);
+}
+.full-screen-image-modal:focus-visible {
+  outline: none;
+}
+.full-screen-image-modal {
+  padding: 0;
+  width: 90vw;
+  background-color: var(--bg-color-800);
+  border: 3px solid var(--bg-color-500);
+  border-radius: 10px;
+}
+.full-screen-image-wrapper {
+  padding: 2rem;
+}
+.modal-close-button-wrapper {
+  cursor: pointer;
+  position: absolute;
+  top: 0;
+  right: 0;
+}
+</style>
+<style>
+.full-screen-image-modal img {
+  height: unset !important;
 }
 </style>
