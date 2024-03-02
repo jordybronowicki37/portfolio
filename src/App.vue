7febdbeae9d8e2b9ad47462a9cb10b35f264a570
jordy <74408184+jordybronowicki37@users.noreply.github.com>
2024-03-02 21:59:24 +0100

    Seperated layout from app

--- src/App.vue
+++ src/App.vue
@@ -1,49 +1,16 @@
 <script setup lang="ts">
-import Header from "./components/Header.vue";
-import SideBar from "./components/SideBar.vue";
-import Footer from "./components/Footer.vue";
 import ThemesManager from "./components/ThemesManager.vue";
-import EditorBackground from "./components/EditorBackground.vue";
+import Layout from "./components/Layout.vue";
 </script>
 
 <template>
   <ThemesManager>
-    <div id="page">
-      <Header />
-      <div id="page-center">
-        <SideBar />
-        <div id="page-content">
-          <EditorBackground/>
-          <router-view />
-        </div>
-      </div>
-      <Footer />
-    </div>
+    <Layout>
+      <router-view/>
+    </Layout>
   </ThemesManager>
 </template>
 
 <style scoped>
-#page {
-  display: flex;
-  flex-direction: column;
-  height: 100vh;
-}
 
-#page-center {
-  position: relative;
-  flex-grow: 2;
-  display: flex;
-  flex-direction: row;
-  max-height: 100%;
-  overflow: hidden;
-}
-
-#page-content {
-  background-color: var(--bg-color-800);
-  display: flex;
-  flex-direction: column;
-  overflow: hidden auto;
-  flex-grow: 2;
-  position: relative;
-}
 </style>
---
+++ src/components/Layout.vue
@@ -0,0 +1,47 @@
+<script setup lang="ts">
+
+import SideBar from "./SideBar.vue";
+import Footer from "./Footer.vue";
+import EditorBackground from "./EditorBackground.vue";
+import Header from "./Header.vue";
+</script>
+
+<template>
+  <div id="page">
+    <Header />
+    <div id="page-center">
+      <SideBar />
+      <div id="page-content">
+        <EditorBackground/>
+        <slot />
+      </div>
+    </div>
+    <Footer />
+  </div>
+</template>
+
+<style scoped>
+#page {
+  display: flex;
+  flex-direction: column;
+  height: 100vh;
+}
+
+#page-center {
+  position: relative;
+  flex-grow: 2;
+  display: flex;
+  flex-direction: row;
+  max-height: 100%;
+  overflow: hidden;
+}
+
+#page-content {
+  background-color: var(--bg-color-800);
+  display: flex;
+  flex-direction: column;
+  overflow: hidden auto;
+  flex-grow: 2;
+  position: relative;
+}
+</style>
