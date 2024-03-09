5de21189da91f26e718cb3b7fa9227ac2428791a
jordy <74408184+jordybronowicki37@users.noreply.github.com>
2024-03-09 10:20:31 +0100

    Fixed some things on contact page and added location

--- src/pages/ContactPage.vue
+++ src/pages/ContactPage.vue
@@ -11,21 +11,37 @@ import EditorLine from "../components/EditorLine.vue";
     <EditorLine :indentation="1">
       <h2>Contact</h2>
     </EditorLine>
+    <EditorLine :indentation="2">
+      Reach out and let's have a cup of coffee.
+    </EditorLine>
     <EditorLine :indentation="2" />
 
     <EditorLine :indentation="2">
       <div class="contact-info-line">
         <div class="contact-item-icon-wrapper">
           <box-icon
-              color="var(--font-color-200)"
-              size="2em"
-              type="logo"
-              name="linkedin-square"
+            color="var(--font-color-200)"
+            size="2em"
+            type="solid"
+            name="map"
+          />
+        </div>
+        <p>Netherlands</p>
+      </div>
+    </EditorLine>
+    <EditorLine :indentation="2">
+      <div class="contact-info-line">
+        <div class="contact-item-icon-wrapper">
+          <box-icon
+            color="var(--font-color-200)"
+            size="2em"
+            type="logo"
+            name="linkedin-square"
           />
         </div>
         <a
-            href="https://www.linkedin.com/in/jordy-bronowicki/"
-            target="_blank"
+          href="https://www.linkedin.com/in/jordy-bronowicki/"
+          target="_blank"
         >jordy-bronowicki</a>
       </div>
     </EditorLine>
@@ -33,15 +49,15 @@ import EditorLine from "../components/EditorLine.vue";
       <div class="contact-info-line">
         <div class="contact-item-icon-wrapper">
           <box-icon
-              color="var(--font-color-200)"
-              size="2em"
-              type="logo"
-              name="github"
+            color="var(--font-color-200)"
+            size="2em"
+            type="logo"
+            name="github"
           />
         </div>
         <a
-            href="https://github.com/jordybronowicki37"
-            target="_blank"
+          href="https://github.com/jordybronowicki37"
+          target="_blank"
         >jordybronowicki37</a>
       </div>
     </EditorLine>
@@ -49,14 +65,15 @@ import EditorLine from "../components/EditorLine.vue";
       <div class="contact-info-line">
         <div class="contact-item-icon-wrapper">
           <box-icon
-              color="var(--font-color-200)"
-              size="2em"
-              name="envelope"
+            color="var(--font-color-200)"
+            size="2em"
+            type="solid"
+            name="envelope"
           />
         </div>
         <a
-            href="mailto:jordybronowicki37@gmail.com"
-            target="_blank"
+          href="mailto:jordybronowicki37@gmail.com"
+          target="_blank"
         >jordybronowicki37@gmail.com</a>
       </div>
     </EditorLine>
@@ -73,11 +90,13 @@ import EditorLine from "../components/EditorLine.vue";
   gap: 0.5rem;
   margin: 0.3rem 0;
 }
+
 .contact-item-icon-wrapper {
   display: flex;
   align-items: center;
   justify-content: center;
 }
+
 a {
   font-size: 1em;
 }
