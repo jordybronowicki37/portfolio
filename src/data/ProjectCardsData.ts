74e367d3d4c17666754df3b56171b550ebc2c89a
jordy <74408184+jordybronowicki37@users.noreply.github.com>
2024-10-25 23:35:36 +0200

    Changed befit status to completed

--- src/data/ProjectCardsData.ts
+++ src/data/ProjectCardsData.ts
@@ -73,7 +73,7 @@ export const projectCardsData: ProjectCardProps[] = [
     "title": "BeFit-bot",
     "imageSrc": "/befit-exercise-progress.png",
     "imageAlt": "A standard exercise progress report",
-    "tags": ["java", "spring", "postgresql", "discord", "personal", "in progress"],
+    "tags": ["java", "spring", "postgresql", "discord", "personal", "completed"],
     "externalLinks": [
       import.meta.env.VITE_BEFIT_GITHUB_URL
     ],
