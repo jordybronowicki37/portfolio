2629020aaf1895f5f131dd0ae24c97945cca042c
jordy <74408184+jordybronowicki37@users.noreply.github.com>
2026-02-01 12:39:36 +0100

    Reworked project properties

--- src/components/ProjectProperties.vue
+++ src/components/ProjectProperties.vue
@@ -9,136 +9,145 @@ defineProps<ProjectCardProps>();
 
 <template>
   <div class="project-properties">
-    <p>Languages:</p>
-    <div class="tag-list-wrapper">
-      <TechPillConfigured
-        v-for="language in tags.languages"
-        :key="language"
-        :type="language"
-      />
+    <div class="property-group">
+      <p>Languages</p>
+      <div class="tag-list-wrapper">
+        <TechPillConfigured
+          v-for="language in tags.languages"
+          :key="language"
+          :type="language"
+        />
+      </div>
     </div>
 
-    <p v-if="tags.frameworks">
-      Frameworks:
-    </p>
     <div
       v-if="tags.frameworks"
-      class="tag-list-wrapper"
+      class="property-group"
     >
-      <TechPillConfigured
-        v-for="framework in tags.frameworks"
-        :key="framework"
-        :type="framework"
-      />
+      <p>Frameworks</p>
+      <div class="tag-list-wrapper">
+        <TechPillConfigured
+          v-for="framework in tags.frameworks"
+          :key="framework"
+          :type="framework"
+        />
+      </div>
     </div>
 
-    <p v-if="tags.integrations">
-      Integrations:
-    </p>
     <div
       v-if="tags.integrations"
-      class="tag-list-wrapper"
+      class="property-group"
     >
-      <TechPillConfigured
-        v-for="integration in tags.integrations"
-        :key="integration"
-        :type="integration"
-      />
+      <p>Integrations</p>
+      <div class="tag-list-wrapper">
+        <TechPillConfigured
+          v-for="integration in tags.integrations"
+          :key="integration"
+          :type="integration"
+        />
+      </div>
     </div>
 
-    <p v-if="tags.database">
-      Database:
-    </p>
     <div
       v-if="tags.database"
-      class="tag-list-wrapper"
+      class="property-group"
     >
-      <TechPillConfigured :type="tags.database" />
+      <p>Database</p>
+      <div class="tag-list-wrapper">
+        <TechPillConfigured :type="tags.database" />
+      </div>
     </div>
 
-    <p v-if="tags.deployment">
-      Deployment:
-    </p>
     <div
       v-if="tags.deployment"
-      class="tag-list-wrapper"
+      class="property-group"
     >
-      <TechPillConfigured :type="tags.deployment" />
+      <p>Deployment</p>
+      <div class="tag-list-wrapper">
+        <TechPillConfigured :type="tags.deployment" />
+      </div>
     </div>
 
-    <p>Type:</p>
-    <div class="tag-list-wrapper">
-      <TechPillConfigured :type="tags.type" />
+    <div class="property-group">
+      <p>Type</p>
+      <div class="tag-list-wrapper">
+        <TechPillConfigured :type="tags.type" />
+      </div>
     </div>
 
-    <p v-if="tags.grade">
-      Grade:
-    </p>
     <div
       v-if="tags.grade"
-      class="tag-list-wrapper"
+      class="property-group"
     >
-      <TechPillSimple color="#555">
-        <p class="grade-text">
-          {{ tags.grade }}
-        </p>
-      </TechPillSimple>
+      <p>Grade</p>
+      <div class="tag-list-wrapper">
+        <TechPillSimple color="#555">
+          <p class="grade-text">
+            {{ tags.grade }}
+          </p>
+        </TechPillSimple>
+      </div>
     </div>
 
-    <p v-if="tags.groupSize">
-      Group size:
-    </p>
     <div
       v-if="tags.groupSize"
-      class="tag-list-wrapper"
+      class="property-group"
     >
-      <TechPillSimple color="#555">
-        <p class="group-size-text">
-          {{ tags.groupSize }}
-        </p>
-      </TechPillSimple>
+      <p>Group size</p>
+      <div class="tag-list-wrapper">
+        <TechPillSimple color="#555">
+          <p class="group-size-text">
+            {{ tags.groupSize }}
+          </p>
+        </TechPillSimple>
+      </div>
     </div>
 
-    <p>Status:</p>
-    <div class="tag-list-wrapper">
-      <TechPillConfigured :type="tags.status" />
+    <div class="property-group">
+      <p>Status</p>
+      <div class="tag-list-wrapper">
+        <TechPillConfigured :type="tags.status" />
+      </div>
     </div>
 
-    <p v-if="externalLinks && externalLinks.length">
-      Links:
-    </p>
     <div
       v-if="externalLinks && externalLinks.length"
-      class="tag-list-wrapper"
+      class="property-group"
     >
-      <TechPillLink
-        v-for="link in externalLinks"
-        :key="link"
-        :link="link"
-      />
+      <p>Links</p>
+      <div class="tag-list-wrapper">
+        <TechPillLink
+          v-for="link in externalLinks"
+          :key="link"
+          :link="link"
+        />
+      </div>
     </div>
   </div>
 </template>
 
-<style scoped>
+<style scoped lang="scss">
 .project-properties {
-  display: grid;
-  grid-template-columns: auto auto;
-  gap: 0.3rem 0.5rem;
-}
-.tag-list-wrapper {
   display: flex;
-  align-items: center;
-  flex-wrap: wrap;
-  gap: 0.3rem;
-}
-.grade-text, .group-size-text {
-  color: var(--font-color-200);
-  padding: 2px 0.5rem;
-  text-transform: uppercase;
-  font-weight: 800;
-  font-size: 10px;
-  white-space: nowrap;
-  user-select: none;
+  flex-direction: column;
+  gap: 0.5rem;
+  p {
+    font-weight: bold;
+  }
+  .tag-list-wrapper {
+    display: flex;
+    align-items: center;
+    flex-wrap: wrap;
+    gap: 0.3rem;
+  }
+  .grade-text, .group-size-text {
+    color: var(--font-color-200);
+    padding: 2px 0.5rem;
+    text-transform: uppercase;
+    font-weight: 800;
+    font-size: 10px;
+    white-space: nowrap;
+    user-select: none;
+  }
 }
 </style>
